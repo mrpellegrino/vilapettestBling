5f9e1d3f7156d08ef21b015d69fb38ab2fa5dade
Douglas Santiago Rodrigues <douglas.rodrigues@taking.com.br>
2023-02-15 14:24:08 -0300

    feat: create home page and app file in src folder

--- src/hooks/useForm.jsx
+++ src/hooks/useForm.jsx
@@ -1,41 +1,41 @@
 "use client"
-import {useState} from 'react';
+import { useState } from 'react';
 import UserForm from '../components/UserForm';
 import PetForm from '../components/PetForm';
 import CaseForm from '../components/CaseForm';
 import Thanks from '../components/Thanks';
 
-export default function useForm(){    
+export default function useForm() {
     const [currentStep, setCurrentStep] = useState(0);
-    const [formState,setFormState]=useState({})
-    
-    function handleChangeField(field,value){
-        setFormState((prevState)=>({...prevState,[field]:value}))
+    const [formState, setFormState] = useState({})
+
+    function handleChangeField(field, value) {
+        setFormState((prevState) => ({ ...prevState, [field]: value }))
     }
 
-    const formComponents = 
-    [
-      <UserForm handleChangeField={handleChangeField} formState={formState}/>,
-      <PetForm handleChangeField={handleChangeField} formState={formState}/>,
-      <CaseForm handleChangeField={handleChangeField} formState={formState}/>,
-      <Thanks /> 
-    ];
-    
-    function changeStep(i, e){
-       if(e) e.preventDefault();
+    const formComponents =
+        [
+            <UserForm handleChangeField={handleChangeField} formState={formState} key='UserForm' />,
+            <PetForm handleChangeField={handleChangeField} formState={formState} key='PetForm' />,
+            <CaseForm handleChangeField={handleChangeField} formState={formState} key='CaseForm' />,
+            <Thanks key='Thanks' />
+        ];
 
-        if(i < 0 || i>= formComponents.length) return;
+    function changeStep(i, e) {
+        if (e) e.preventDefault();
+
+        if (i < 0 || i >= formComponents.length) return;
 
         setCurrentStep(i);
     }
 
 
-    return{
+    return {
         currentStep,
         currentComponent: formComponents[currentStep],
         changeStep,
-        isLastStep: currentStep+1 === formComponents.length? true : false,
-        isFirstStep: currentStep === 0? true : false,
+        isLastStep: currentStep + 1 === formComponents.length ? true : false,
+        isFirstStep: currentStep === 0 ? true : false,
         handleChangeField,
         formState
     }
---
+++ src/pages/index.tsx
@@ -0,0 +1,46 @@
+import useForm from "@/hooks/useForm"
+import '../styles/Home.css'
+
+import { GrFormNext, GrFormPrevious, GrFormSubtract } from 'react-icons/gr';
+import { FiSend } from 'react-icons/fi';
+import Steps from "@/components/Steps/Steps";
+
+export default function Home() {
+
+  const { currentStep,
+    currentComponent,
+    changeStep,
+    isLastStep,
+    isFirstStep
+  } = useForm();
+  return (
+    <div className="app">
+      <div className='header'>
+
+        Cadastro de Clientes Vila Pet Brasil
+      </div>
+      <p>Esse cadastro respeita A Lei Geral de ProteÃ§Ã£o de Dados Pessoais.
+        Os dados informados sÃ£o usados somente para cumprimento de obrigaÃ§Ãµes fiscais e cadastro</p>
+
+      <div className='form-container'>
+        <Steps currentStep={currentStep} />
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
+          <div className='inputs-container'>{currentComponent}</div>
+          <div className="actions">
+            {isFirstStep ? (<span></span>) : (<button type='button' onClick={() => changeStep(currentStep - 1)}>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>)}
+            {!isLastStep ? (<button type='submit'>
+              <span>AvanÃ§ar</span>
+              <GrFormNext />
+            </button>) : (<button type='button'>
+              <span>Enviar</span>
+              <FiSend />
+            </button>)}
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
