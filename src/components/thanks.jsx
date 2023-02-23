48c87c9f2ef421de5cae447598424aa25be371bd
Dayvson <dayvson.pellegrinorodrigues@gmail.com>
2023-02-23 15:31:07 -0300

    Built mudança na url

--- src/components/thanks.jsx
+++ src/components/thanks.jsx
@@ -10,7 +10,7 @@ import Head from 'next/head';
         const thanks = ({handleChangeField,formState}) => {
           const name = formState.name;
           const cpf = formState.cpf;
-          const url= "5; url='https://wa.me/5531975166552?text=Concluí meu cadastro com sucesso. Nome:*"+formState.name+"* Cpf:*"+formState.cpf+"*";
+          const url= "5; url='https://wa.me/5531975166552?text=Concluí meu cadastro com sucesso. Nome:"+formState.name+" Cpf:"+formState.cpf;
         return (
           <>
         <div>  <h2>Muito Obrigado pelo seu cadastro!</h2>
