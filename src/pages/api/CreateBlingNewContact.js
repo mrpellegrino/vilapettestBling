f8cbe730b7c331b717b4f60fcb7cba4b17314a3c
Dayvson <dayvson.pellegrinorodrigues@gmail.com>
2023-03-13 20:52:03 -0300

    commit

--- src/pages/api/CreateBlingNewContact.js
+++ src/pages/api/CreateBlingNewContact.js
@@ -5,6 +5,7 @@ async function CreateBlingNewContact(apiKey, obj) {
   xml += '<contato>\n';
   xml += `<nome>${obj.name}</nome>\n`;
   xml += `<fantasia>${obj.petName}</fantasia>\n`;
+  xml += `<tipoPessoa>F</tipoPessoa>\n`;
   xml += `<contribuinte>9</contribuinte>\n`;
   xml += `<cpf_cnpj>${obj.cpf}</cpf_cnpj>\n`;
   xml += `<endereco>${obj.address}</endereco>\n`;
@@ -12,20 +13,16 @@ async function CreateBlingNewContact(apiKey, obj) {
   xml += `<complemento>${obj.addressComplement}</complemento>\n`;
   xml += `<bairro>${obj.neighborhood}</bairro>\n`;
   xml += `<cep>${obj.zipCode}</cep>\n`;
-  xml += `<cidade>${obj.city}</cidade>\n`;
+  xml += `<cidade>${obj.city}</cidade>\n`;  
   xml += `<uf>${obj.uf}</uf>\n`;
   xml += `<fone>${obj.telephone}</fone>\n`;
   xml += '</contato>\n';
  
 
 
-      const url = 'https://bling.com.br/Api/v2/contato/';
-      const formData = new URLSearchParams();
-      formData.append('apikey', apiKey);
-      formData.append('xml', xml);
+      const url = 'https://bling.com.br/Api/v2/contato?apikey'+{apiKey}+'?xml'+{xml};
       const response = await fetch(url, {
         method: 'POST',
-        body: formData,
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         }
